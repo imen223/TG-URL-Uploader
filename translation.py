f3e335e4f9c7fde7927e40655af236b45b960d85
che369 <63643345+che369@users.noreply.github.com>
2020-06-06 13:54:35 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,13 +1,5 @@
 class Translation(object):
-    START_TEXT = """Hello,
-This is a Telegram URL Upload Bot!
-
-<b>Please send me any direct download URL Link, i can upload to telegram as File/Video</b>
-
-/help for more details..
-
-Support Group : @InFoTelGroup
-© @SpEcHlDe , @TGBotsZ & @CWProjects"""
+    START_TEXT = """Hi,\n\nI'm <b>Uploadz bot</b>. Just send me any Direct download link and I'll upload file remotely to Telegram....\n\nFor more details hit /help."""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
     UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
@@ -15,12 +7,12 @@ Support Group : @InFoTelGroup
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
     NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
-    DOWNLOAD_START = "trying to download"
-    UPLOAD_START = "trying to upload"
+    DOWNLOAD_START = "<b>📥Downloading to My Server📥</b>"
+    UPLOAD_START = "<b>📤Uploading to Telegram📤</b>"
     RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "Please rate me if you find me useful. Join : @TGBotsZ"
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds. \nJoin : @TGBotsZ \nUploaded in {} seconds."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "<b>Thank you for using me🤗\nJoin : @MalluBotz</b>"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = " <b>➠ Downloaded in : {} seconds. \n➤ Join : @MalluBotz \n➠ Uploaded in : {} seconds.</b>"
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ThankTelegram'>@SpEcHlDe</a>"
     SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
@@ -36,23 +28,34 @@ URL | filename | username | password"""
 Telegram ID: <code>{}</code>
 Plan name: Free Cloned User
 Expires on: 31/12/2020"""
-    HELP_USER = """Hai am URL Uploader bot..
-    
-1. Send url (Link|New Name with Extension).
-2. Send Custom Thumbnail (Optional).
-3. Select the button.
-   SVideo - Give File as video with Screenshots
-   DFile  - Give File with Screenshots
-   Video  - Give File as video without Screenshots
-   DFile  - Give File without Screenshots
-   
-<b>👉 Create own Clone Bot :</b> 👉 <a href="https://youtu.be/QkAkSLBgoYw">Diploy</a>
+    HELP_USER = """<b>Hai I'm Uploadz bot..</b>
 
---------
+👉 <u>How To Direct Upload File/Video</u>
+
+1. Send url (Link|New Name.Extension)
+2. Send Custom Thumbnail (Optional)
+3. Select the button
+    ➥ <i>SVideo - Give File as video</i>
+    ➥ <i>DFile  - Give File</i>
+    ➥ <i>Video  - Give File as video</i>
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+👉 <u>How To Rename TG File</u>
+
+1. Send/Forward TG File
+2. Reply /rename (New Name.Extension)
+3. Send Custom Thumbnail
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+👉 <u>How To Download Youtube Videos</u>
+
+1. Send/Forward YouTube Link
+2. Select the button
+    ➥ <i>SVideo - Give File as video</i>
+    ➥ <i>DFile  - Give File</i>
+    ➥ <i>MP3  - Give File as Audio</i>
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Send /me to know current plan details
 
-Support Group : @InFoTelGroup
-© @TGBotsZ & @CWProjects"""
+Support Channel : @MalluBotz"""
     REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media to get High Speed Direct Download Link"
     REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
     REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
